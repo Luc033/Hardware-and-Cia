f4afdec222189112f6dbf4686f830f52f17042f4
Henrique Bacarin Litoldo <henriquebacarin@gmail.com>
2023-05-05 20:42:26 -0300

    Atualização Javascrip, filter e responsividade

--- js/func-logado.js
+++ js/func-logado.js
@@ -53,52 +53,16 @@ produtos.map((produto) => {
           <h1>${produto.name}</h1>
           <h2>R$${produto.price}</h2>
           <p>À vista no PIX</p>
-          <a href="#" class="btn card-btn" style="display: block"><i class="bi bi-cart3"></i> Comprar</a>
+          <div class="branco"></div>
+          <a href="#" class="btn card-btn"><i class="bi bi-cart3"></i> Comprar</a>
         </div>
     </div>
     ` 
-    return true
 })
 
-const filtro = document.querySelector("#filter-btn")
 
-filtro.addEventListener("click", () => {
-  
-
-    console.log("funfo")
-
-    const range1 = document.querySelector("#slider-1").value
-    const range2 = document.querySelector("#slider-2").value
-  
-    const menorPreco = document.querySelector("#menorPreco")
-    const maiorPreco = document.querySelector("#maiorPreco")
-  
-    console.log(range1, range2, menorPreco, maiorPreco)
-  
-    containerProducts.innerHTML = ""
-  
-    produtos.filter((produto) => {
-
-      if(range1 < produto.price && produto.price < range2) {
-      containerProducts.innerHTML += 
-      `
-      <div class="card" />
-        <img class="card-img-top" src="imagens/${produto.imageURL}" alt="${produto.name}">
-          <div class="card-body">
-            <h1>${produto.name}</h1>
-            <h2>R$${produto.price}</h2>
-            <p>À vista no PIX</p>
-            <a href="#" class="btn card-btn" style="display:block"><i class="bi bi-cart3"></i> Comprar</a>
-          </div>
-      </div>
-      ` 
-      } else {
-        return
-      }
-    })
-  }
-)
  
+
 //mudando componentes de responsividade do site
 let largura = document.querySelector("body").clientWidth
 let imagemLogo = document.querySelector(".logo-header")
@@ -107,6 +71,7 @@ let inputSearch = document.querySelector(".inputSearch")
 let filter = document.querySelector(".btn-filter")
 const newI = document.createElement("i")
 newI.className = "bi bi-filter"
+
 
 if(largura < 500) {
 
@@ -120,6 +85,29 @@ if(largura < 500) {
 } else {
   imagemLogo.setAttribute("src", "imagens/logo-sombra.png");
 }
+
+//botão aparecendo após o hover 
+const cards = document.querySelectorAll(".card")
+
+
+cards.forEach(function(card) {
+
+  card.addEventListener("mouseover", function() {
+    const btn = this.querySelector(".card-btn")
+    btn.style.display = "block"
+
+    const branco = this.querySelector(".branco")
+    branco.style.display = "none"
+  })
+  
+  card.addEventListener("mouseout", function() {
+    const btn = this.querySelector(".card-btn")
+    btn.style.display = "none"
+
+    const branco = this.querySelector(".branco")
+    branco.style.display = "block"
+  })
+})
 
 //range filter
 window.onload = function () {
@@ -159,9 +147,44 @@ function fillColor () {
   sliderTrack.style.background = `linear-gradient(to right, #dadae5 ${percent1}%, #f95738 ${percent1}%, #f95738  ${percent2}%, #dadae5 ${percent2}%)`
 }
 
+//Adicionando o dropdown do botão "Relatório"
 
+// selecione o botão e o submenu
+const btnRelatorio = document.getElementById("btn-dropd-relatorio");
+const submenuRelatorio = document.querySelector(".submenu-relatorio");
 
+// adicione um evento de clique ao botão
+btnRelatorio.addEventListener("click", function() {
+  // verifique se o submenu está visível
+  if (submenuRelatorio.style.display === "block") {
+    // se estiver visível, oculte-o
+    submenuRelatorio.style.display = "none";
+  } else {
+    // se estiver oculto, exiba-o
+    submenuRelatorio.style.display = "block";
+  }
+});
 
+const filtro = document.querySelector("#filter-btn")
+
+const handleSumbit = (e) => {
+
+  console.log("funfo")
+
+  const range1 = document.querySelector("#slider-1") 
+  const range2 = document.querySelector("#slider-2") 
+
+  const menorPreco = document.querySelector("#menorPreco")
+  const maiorPreco = document.querySelector("#maiorPreco")
+
+  produtos.map((produto) => {
+    if(range1 < produto.price < range2) {
+      return true
+    } else {
+      produto.style.display = "none"
+    }
+  })
+}
 
 
 
